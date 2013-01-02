ad2d8f326e8b8dfa63359ca55e07a9a16dcf435f
kylehuff <code@curetheitch.com>
2013-01-02 18:29:53 -0500

    Changes for v0.9.2
    * Added support for named groups
    * Added search ability to Encrypt-to key dialog
    * Added review dialog for import of public keys
    * Improved GMAIL compose integration
    * Added support for new GMAIL compose window (for in4mer, enjoy!)
    * Added WebPG action menu to text-area fields [BETA]
    * Fixed Firefox options dialog sizing and decoration
    * Added ability to publish keys to defined key-server
    * Added ability to retrieve keys from defined key-server
    * Added Email and UID aliases to Encrypt-to dialog
    * Minor bug fix to key expiration dialog
    * Moved to webpg-npapi v0.6.3

--- defaults/preferences/webpg-firefox.js
+++ defaults/preferences/webpg-firefox.js
@@ -1,3 +1,4 @@
 pref("extensions.webpg-firefox.decorate_inline", true);
 pref("extensions.webpg-firefox.gmail_integration", false);
 pref("extensions.webpg-firefox.sign_gmail", false);
+pref("extensions.webpg-firefox.keyserver", "");
