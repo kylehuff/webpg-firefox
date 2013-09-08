68dfa6eceea76e2a7d69e03e2aefe5da86baeb18
kylehuff <code@curetheitch.com>
2013-09-08 13:58:26 -0400

    Removed README from config_build.sh

--- config_build.sh
+++ config_build.sh
@@ -4,7 +4,7 @@ VERSION=`sed -n 's/.*em\:version\=\"\(.*\)\"/\1/p' install.rdf`
 APP_NAME=webpg-firefox-v${VERSION}
 CHROME_PROVIDERS="extension locale extension/skin"
 CLEAN_UP=1
-ROOT_FILES="AUTHORS COPYING README"
+ROOT_FILES="AUTHORS COPYING"
 ROOT_DIRS="defaults plugins"
 BEFORE_BUILD=
 AFTER_BUILD="firefox ${APP_NAME}.xpi"
